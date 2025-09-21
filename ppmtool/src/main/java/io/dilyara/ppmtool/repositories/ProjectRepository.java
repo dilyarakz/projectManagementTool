c098dad00f4eb7203548388ba9c2659bf383b48c
Tulegenova, Dilyara <dtulegenova3@gatech.edu>
2025-09-20 21:52:24 -0400

    first commit, created Project entity, tested on Postman, H2 db

---
+++ ppmtool/src/main/java/io/dilyara/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,12 @@
+package io.dilyara.ppmtool.repositories;
+
+import io.dilyara.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
---
+++ ppmtool/src/main/java/io/dilyara/ppmtool/web/ProjectController.java
@@ -0,0 +1,24 @@
+package io.dilyara.ppmtool.web;
+
+import io.dilyara.ppmtool.domain.Project;
+import io.dilyara.ppmtool.services.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/project")
+public class ProjectController {
+    @Autowired
+    private ProjectService projectService;
+
+    @PostMapping("")
+    public ResponseEntity<Project> createProject(@RequestBody Project project){
+        Project project1 = projectService.saveOrUpdateProject(project);
+        return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+    }
+}
