77c9634946f4adaf0a72ef5b9715291aac7da182
Tulegenova, Dilyara <dtulegenova3@gatech.edu>
2025-09-20 23:21:17 -0400

    Improved error message if input is invalid

--- ppmtool/src/main/java/io/dilyara/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/io/dilyara/ppmtool/web/ProjectController.java
@@ -2,9 +2,11 @@ package io.dilyara.ppmtool.web;
 
 import io.dilyara.ppmtool.domain.Project;
 import io.dilyara.ppmtool.services.ProjectService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +19,10 @@ public class ProjectController {
     private ProjectService projectService;
 
     @PostMapping("")
-    public ResponseEntity<Project> createProject(@RequestBody Project project){
+    public ResponseEntity<?> createProject(@Valid @RequestBody Project project, BindingResult result) {
+        if (result.hasErrors()) {
+            return new ResponseEntity<String>("Invalid Project Object", HttpStatus.BAD_REQUEST);
+        }
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
