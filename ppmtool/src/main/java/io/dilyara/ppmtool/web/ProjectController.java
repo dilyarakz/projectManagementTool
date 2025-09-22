eec161574af5d11f4f32c4f72700b8a0fc04d157
Tulegenova, Dilyara <dtulegenova3@gatech.edu>
2025-09-21 21:34:34 -0400

    Created Custom Exception and refactored controller

--- ppmtool/src/main/java/io/dilyara/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/io/dilyara/ppmtool/web/ProjectController.java
@@ -1,6 +1,7 @@
 package io.dilyara.ppmtool.web;
 
 import io.dilyara.ppmtool.domain.Project;
+import io.dilyara.ppmtool.services.MapValidationErrorService;
 import io.dilyara.ppmtool.services.ProjectService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,17 +23,14 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     @PostMapping("")
     public ResponseEntity<?> createProject(@Valid @RequestBody Project project, BindingResult result) {
-        if (result.hasErrors()) {
-            Map<String, String> errorMap = new HashMap<>();
-            for (FieldError error: result.getFieldErrors()) {
-                errorMap.put(error.getField(), error.getDefaultMessage());
-            }
-            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
-        }
 
-
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if (errorMap != null) return errorMap;
 
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
