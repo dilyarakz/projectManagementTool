fe46ca9ff33859bc8e989302832877fe067d5316
Tulegenova, Dilyara <dtulegenova3@gatech.edu>
2025-09-21 21:52:26 -0400

    Implemented getProjectbyid method

--- ppmtool/src/main/java/io/dilyara/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/io/dilyara/ppmtool/web/ProjectController.java
@@ -9,10 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -35,4 +32,11 @@ public class ProjectController {
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
+
+    @GetMapping("/{projectId}")
+    public ResponseEntity<?> getProjectById(@PathVariable String projectId) {
+        Project project = projectService.findProjectByIdentifier(projectId.toUpperCase());
+        return new ResponseEntity<Project>(project, HttpStatus.OK);
+    }
+
 }
