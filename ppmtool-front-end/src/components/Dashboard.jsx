bde982e6f6271380e0498e4b29724e0fc62614b1
Tulegenova, Dilyara <dtulegenova3@gatech.edu>
2025-12-14 01:03:47 -0500

    Created Components and Navbar

---
+++ ppmtool-front-end/src/components/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { ProjectItem } from './Project/ProjectItem'
+
+export const Dashboard = () => {
+  return (
+    <div>
+      <h1>Welcome to the Dashboard</h1>
+      <ProjectItem/>
+    </div>
+  )
+}
